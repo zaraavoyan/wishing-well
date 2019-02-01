3ac152fc3906e9cdb6b615725e0fa14e8d488337
J.D. Zamfirescu <zamfi@alum.mit.edu>
2019-02-01 01:19:01 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+var wisdoms = [
+  "Semper Ubi Sub Ubi. (Always wear underwear.)",
+  "Floss your teeth every day.",
+  "You will pay for your sins. If you have already paid, please disregard this message.",
+  "Today is a day for firm decisions!! Or is it?",
+  "Caution: Keep out of reach of children.",
+  "You're growing out of some of your problems, but there are others that you're growing into.",
+  "Every cloud engenders not a storm."
+]
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    var index = Math.floor(Math.random() * wisdoms.length);
+    
+    this.state = {
+      wisdom: wisdoms[index]
+    };
+    
+    this.setRandomWisdom = this.setRandomWisdom.bind(this);
+    this.addWisdom = this.addWisdom.bind(this);
+  }
+  
+  setRandomWisdom() {
+    var index = Math.floor(Math.random() * wisdoms.length);
+    
+    this.setState({
+      wisdom: wisdoms[index]
+    });
+  }
+  
+  addWisdom() {
+    wisdoms.push(prompt("What new wisdom do you offer?"));
+  }
+  
+  removeCurrentWisdom() {
+    var index = wisdoms.indexOf(this.state.wisdom);
+    wisdoms.splice(index, 1);
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.wisdom}
+        <button className="more" onClick={this.setRandomWisdom}>Another</button>
       </div>
     );
   }
